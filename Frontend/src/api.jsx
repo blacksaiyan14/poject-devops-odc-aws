a211fe61a1f2c8e9c9ed420f2c324e4ef7c94098
Black_sayan <cissetaif3@gmail.com>
2025-05-25 20:17:13 +0000

    partie ansible du projet fil rouge

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://odc.local/api/",
+    baseURL: "http://api.local/api/",
 });
 
 export const UseAxios = () => {
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://odc.local/api/', 
+  baseURL: 'http://api.local/api/', 
 });
 
 
